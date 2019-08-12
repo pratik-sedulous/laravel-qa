c31395f0eac6825304063902668fe2cee1f4a3dd
pratik-sedulous <pratik.sedulous@gmail.com>
2019-08-12 16:59:08 +0530

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\AskQuesionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+
     /**
      * Display a listing of the resource.
      *
@@ -64,16 +70,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        /*
-        if(\Gate::allows('update-question', $question)) {
-            return view('questions.edit', compact('question'));
-        }
-        abort(403, "Access denied");
-        */
-
-        if(\Gate::denies('update-question', $question)) {
-            abort(403, "Access denied");
-        }
+        $this->authorize('update', $question);
         return view('questions.edit', compact('question'));
     }
 
@@ -86,10 +83,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuesionRequest $request, Question $question)
     {
-        if(\Gate::denies('update-question', $question)) {
-            abort(403, "Access denied");
-        }
-
+        $this->authorize('update', $question);
         $question->update($request->only('title', 'body'));
 
         return redirect()->route('questions.index')->with('success', "Your question has been updated.");
@@ -103,10 +97,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if(\Gate::denies('delete-question', $question)) {
-            abort(403, "Access denied");
-        }
-
+        $this->authorize('delete', $question);
         $question->delete();
 
         return redirect()->route('questions.index')->with('success', "Your question has been deleted.");
