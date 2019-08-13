6401986f819dba36d6cd5ccf59d7ee735d5296fb
pratik-sedulous <pratik.sedulous@gmail.com>
2019-08-13 16:45:34 +0530

    Add vote controls to question and answer item

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,5 @@
+import fontawesome from '@fortawesome/fontawesome-free';
+import { library, icon } from '@fortawesome/fontawesome-svg-core';
+import { faCaretUp, faCaretDown, faStar, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+library.add([faCaretUp, faCaretDown, faStar, faCheck])
